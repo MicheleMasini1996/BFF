115a749d1dc2dc90dff644a4a368d42b6f49f502
MicheleMasini1996 <45631570+MicheleMasini1996@users.noreply.github.com>
2024-02-20 12:10:47 +0100

    Delete QuantumNPA.jl-bff directory

--- QuantumNPA.jl-bff/src/npa.jl
+++
@@ -1,6 +0,0 @@
-# Helper code dealing with constructing operators at a given hierarchy level
-# and parsing levels like "1 + A B".
-include("npa_level.jl")
-
-# Code to build moment matrix, convert NPA -> SDP, and solve SDP.
-include("npa_impl.jl")
